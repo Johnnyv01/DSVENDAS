01c5b01c840b35d6df7414f59e3ac654ac0756ba
Jonathan Vicente <jonathan-priston@hotmail.com>
2021-09-09 13:22:59 -0300

    Project crated

---
+++ backend/src/test/java/com/dsvendas/BIP/BipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsvendas.BIP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
